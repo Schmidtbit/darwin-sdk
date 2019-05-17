171479200f5ec4242e7bb8352b58a31a1903810f
Sheila Cheng <scheng@sparkcognition.com>
2019-05-16 20:39:27 -0500

    update for darwin release 1.7.0

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 from os import path
+from amb_sdk.version import __version__
 
 setup(
     name='amb_sdk',
@@ -7,7 +8,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0',
+    version=__version__,
 
     description='The AMB SDK is a python client for the AMB API.',
 
@@ -18,15 +19,11 @@ setup(
     author='Sheila Cheng',
     author_email='scheng@sparkcognition.com',
     install_requires=[
-        'validators',
-        'urllib3',
-        'pandas',
-        'requests_toolbelt',
-        'requests',
-        'matplotlib',
-        'scikit-learn',
-        'scipy',
-        'jupyter'
+        'validators==0.12.4',
+        'urllib3==1.24.1',
+        'pandas==0.24.2',
+        'requests_toolbelt==0.9.1',
+        'requests==2.21.0',
     ],
     packages=['amb_sdk']
 )
